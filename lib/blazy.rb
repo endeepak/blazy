bfd3f3a76c21a847ff972ddd4a2dd504aac4a7ed
Deepak N <endeep123@gmail.com>
2010-10-03 16:45:35 +0530

    Fixing path issue for active model

--- lib/blazy.rb
+++ lib/blazy.rb
@@ -2,6 +2,6 @@ require 'rubygems'
 require 'active_record'
 require 'active_support/inflector'
 
-require 'active_model'
+require 'extensions/active_model'
 require 'extensions/active_record'
 require 'extensions/class'
