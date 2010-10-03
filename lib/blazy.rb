20199ba6b8add1d3b32ae715464da56f1cc1a232
Deepak N <endeep123@gmail.com>
2010-10-03 20:23:09 +0530

    Fixed missing require file and Blazy now loads all rails models so the ActiveRecord:: Base.subclasses returns all models

--- lib/blazy.rb
+++ lib/blazy.rb
@@ -4,4 +4,5 @@ require 'active_support/inflector'
 
 require 'extensions/active_model'
 require 'extensions/active_record'
-require 'extensions/class'
+
+Dir.glob(File.join(RAILS_ROOT,'app','models','**','*.rb')).each { |file| require_dependency file } if defined?(RAILS_ROOT)
